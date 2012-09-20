a912b2a6acdcc1b2bfb7e904d255d934dff6dbb3
valex <valex@localhost>
2012-09-20 16:32:27 +0400

    SpdifWrapper2 replaces old SPDIFWrapper.

--- test2/tests/parsers/spdif/test_spdif_wrapper.cpp
+++ test2/tests/parsers/spdif/test_spdif_wrapper.cpp
@@ -7,7 +7,7 @@
 #include "parsers/dts/dts_header.h"
 #include "parsers/spdif/spdif_header.h"
 #include "parsers/spdif/spdif_parser.h"
-#include "parsers/spdif/spdif_wrapper2.h"
+#include "parsers/spdif/spdif_wrapper.h"
 #include "parsers/spdif/spdifable_header.h"
 #include "source/file_parser.h"
 #include "../../../suite.h"
@@ -49,7 +49,7 @@ static void compare_file(const char *raw_file, const char *spdif_file)
   f_spdif.open_probe(spdif_file, &frame_parser_spdif);
   BOOST_REQUIRE(f_spdif.is_open());
 
-  SpdifWrapper2 spdifer;
+  SpdifWrapper spdifer;
   compare(&f_raw, &spdifer, &f_spdif, 0);
 }
 
@@ -62,7 +62,7 @@ static void test_streams_frames(const char *filename, int streams, int frames)
   f.open_probe(filename, &frame_parser);
   BOOST_REQUIRE(f.is_open());
 
-  SpdifWrapper2 parser;
+  SpdifWrapper parser;
   parser.open(f.get_output());
   BOOST_CHECK(parser.is_open());
 
@@ -73,21 +73,21 @@ BOOST_AUTO_TEST_SUITE(spdif_wrapper)
 
 BOOST_AUTO_TEST_CASE(constructor)
 {
-  SpdifWrapper2 spdifer;
+  SpdifWrapper spdifer;
   BOOST_CHECK_EQUAL(spdifer.dts_mode, DTS_MODE_AUTO);
   BOOST_CHECK_EQUAL(spdifer.dts_conv, DTS_CONV_NONE);
 }
 
 BOOST_AUTO_TEST_CASE(init_constructor)
 {
-  SpdifWrapper2 spdifer(DTS_MODE_WRAPPED, DTS_CONV_14BIT);
+  SpdifWrapper spdifer(DTS_MODE_WRAPPED, DTS_CONV_14BIT);
   BOOST_CHECK_EQUAL(spdifer.dts_mode, DTS_MODE_WRAPPED);
   BOOST_CHECK_EQUAL(spdifer.dts_conv, DTS_CONV_14BIT);
 }
 
 BOOST_AUTO_TEST_CASE(can_open)
 {
-  SpdifWrapper2 spdifer;
+  SpdifWrapper spdifer;
   BOOST_CHECK(spdifer.can_open(Speakers(FORMAT_AC3, 0, 0)));
   BOOST_CHECK(spdifer.can_open(Speakers(FORMAT_DTS, 0, 0)));
   BOOST_CHECK(spdifer.can_open(Speakers(FORMAT_MPA, 0, 0)));
@@ -216,7 +216,7 @@ BOOST_AUTO_TEST_CASE(dts_options)
     BOOST_REQUIRE(f_ref.is_open());
 
     DTSFrameResize resize(tests[i].frame_size);
-    SpdifWrapper2 spdifer(tests[i].dts_mode, tests[i].dts_conv);
+    SpdifWrapper spdifer(tests[i].dts_mode, tests[i].dts_conv);
     SPDIFParser despdifer;
 
     FilterChain test(&resize, &spdifer);
